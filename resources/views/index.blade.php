00f14f375c1816c39146c22c262b644ad62fcedb
wan <wanhuz96@gmail.com>
2022-11-26 14:57:52 +0800

    Fix css wrong link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Keep-it</title>
-    <link rel="stylesheet" href="{{asset('public/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('public/css/style.css')}}">
-    <link rel="stylesheet" href="{{asset('public/css/bootstrap-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap-icons.css')}}">
 </head>
 <body>
 
