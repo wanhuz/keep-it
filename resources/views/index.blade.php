b62ae490327e31f756a162a85ec6fcb9f211c0a5
wanhuz <wanhuz96@gmail.com>
2023-05-19 11:33:11 +0800

    Make add card input UI responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -114,36 +114,40 @@
             
             <main class="col mt-4 main-container">
                 <div class="container-fluid">
-                    <div class="mx-auto mb-3" id="editor">
-                        <div id="simpleEditor" class="input-group mb-3 "> 
-                            <input type="text" class="form-control shadow-none" placeholder="Take a note.." >
-                            <button class="btn btn-light btn-outline-secondary" id="cancel-btn"><i class="bi bi-list-task"></i></button>
-                            <button class="btn btn-light btn-outline-secondary"><i class="bi bi-image"></i></button>
-                        </div>
 
+                    <div id="editor" class="row ms-auto mx-auto mb-3">
                         <!-- Iframe will prevent page from breaking when closing the editor -->
                         <iframe name="dummyframe" id="dummyframe" style="display: none;"></iframe> 
-                        <div class="d-none" id="fullEditor" >
-                            <div class="card">
-                                <form target="dummyframe" id="postform">
-                                    @csrf
-                                    <div id="fullEditorCard" class="card-body" >
-                                            <input type="text" name="title" id="titleTextArea" class="form-control border border-0 shadow-none" placeholder="Title">
-                                            <textarea name="body" id="bodyTextArea" class="form-control border border-0 shadow-none" placeholder="Take a note.." id="fullEditorTextArea" cols="30" rows="3"></textarea>
-                                    </div>
-                                    <div class="card-footer d-flex flex-row">
-                                        <button class="btn btn-light btn-outline-secondary border border-0"><i class="bi bi-list-task"></i></button>
-                                        <button class="btn btn-light btn-outline-secondary border border-0"><i class="bi bi-image"></i></button>
-                                        
-                                        <button class="btn btn-light border border-0 ms-auto" id="cancelBtn"><i class="bi bi-x"></i></button>
-                                        <button type="submit" class="btn btn-light border border-0" id="submitBtn"><i class="bi bi-check-lg"></i></button>
-                                    </div>
-                                </form>
+
+                        <div class="col-sm-3 mx-auto"> 
+                            <div id="simpleEditor" class="input-group mb-3 "> 
+                                <input type="text" class="form-control shadow-none" placeholder="Take a note.." >
+                                <button class="btn btn-light btn-outline-secondary" id="cancel-btn"><i class="bi bi-list-task"></i></button>
+                                <button class="btn btn-light btn-outline-secondary"><i class="bi bi-image"></i></button>
+                            </div>
+
+                            <div class="d-none" id="fullEditor" >
+                                <div class="card">
+                                    <form target="dummyframe" id="postform">
+                                        @csrf
+                                        <div id="fullEditorCard" class="card-body" >
+                                                <input type="text" name="title" id="titleTextArea" class="form-control border border-0 shadow-none" placeholder="Title">
+                                                <textarea name="body" id="bodyTextArea" class="form-control border border-0 shadow-none" placeholder="Take a note.." id="fullEditorTextArea" cols="30" rows="3"></textarea>
+                                        </div>
+                                        <div class="card-footer d-flex flex-row">
+                                            <button class="btn btn-light btn-outline-secondary border border-0"><i class="bi bi-list-task"></i></button>
+                                            <button class="btn btn-light btn-outline-secondary border border-0"><i class="bi bi-image"></i></button>
+                                            
+                                            <button class="btn btn-light border border-0 ms-auto" id="cancelBtn"><i class="bi bi-x"></i></button>
+                                            <button type="submit" class="btn btn-light border border-0" id="submitBtn"><i class="bi bi-check-lg"></i></button>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="d-flex flex-row flex-wrap" id="card-container">
+                    <div id="card-container" class="row flex-wrap">
                         @foreach ($notes as $note)
                             <x-card>
                                 <x-slot:id>{{$note->id}}</x-slot>
@@ -153,6 +157,7 @@
                             </x-card>
                         @endforeach
                     </div>
+
                 </div>
             </main>
 
